253096566fbcea7f94627d2cfbfd360161360c13
firebreather-heart <dtenny95@gmail.com>
2024-02-24 11:20:50 +0100

    auth

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User,Profile
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email','is_active','is_staff','is_superuser']
+    list_filter = ['is_active','is_staff','is_superuser']

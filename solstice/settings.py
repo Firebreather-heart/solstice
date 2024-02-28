207b7d82f33e3fc02c333490dae7db993efa0d8e
firebreather-heart <dtenny95@gmail.com>
2024-02-28 07:00:12 +0100

    changed to sqlite db momentarily

--- solstice/settings.py
+++ solstice/settings.py
@@ -92,13 +92,17 @@ WSGI_APPLICATION = 'solstice.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'solstice',
+    #     'USER': 'solsticemaker',
+    #     'PASSWORD': 'solsticepass',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'solstice',
-        'USER': 'solsticemaker',
-        'PASSWORD': 'solsticepass',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 

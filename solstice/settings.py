ab1bbb9fe7bad46a2cb8dd0c3ca6925c002f1be7
firebreather-heart <dtenny95@gmail.com>
2024-03-02 09:06:59 +0100

    minor error

--- solstice/settings.py
+++ solstice/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
